bbb24ea61a8844a3e08c78e8b082ea0d4fe35fbf
James Bergstra <james.bergstra@gmail.com>
2012-08-29 16:23:57 -0400

    ENH: reorganizing

--- setup0.sh
+++ setup0.sh
@@ -2,26 +2,12 @@
 
 set -e
 
-apt-get install vim git
-
 # -- replace all instances of squeeze with unstable
-SOURCES=/etc/apt/sources.list
-cp $SOURCES $SOURCES.orig
-
-echo "deb http://ftp.us.debian.org/debian/ unstable main contrib non-free" > $SOURCES
-echo "deb-src http://ftp.us.debian.org/debian/ unstable main contrib non-free" >> $SOURCES
+mv ./sources.list /etc/apt/sources.list
 
 # -- upgrade to unstable
 apt-get update
 apt-get dist-upgrade
 
-apt-get install firmware-realtek
+. setup1.sh
 
-apt-get install nvidia-cuda-dev
-apt-get install gcc-4.4 g++-4.4
-apt-get install libopenblas-base libopenblas-dev liblapack3 liblapack-dev
-apt-get install libjpeg62 libjpeg62-dev
-apt-get install mongodb
-apt-get install python2.6-dev
-apt-get install python-pip
-apt-get install python-virtualenv virtualenvwrapper
---
+++ setup1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+apt-get install vim
+apt-get install git
+apt-get install firmware-realtek
+apt-get install nvidia-cuda-dev
+apt-get install gcc-4.4 g++-4.4
+apt-get install libopenblas-base libopenblas-dev liblapack3 liblapack-dev
+apt-get install libjpeg62 libjpeg62-dev
+apt-get install mongodb
+apt-get install python2.6-dev
+apt-get install python-pip
+apt-get install python-virtualenv virtualenvwrapper
