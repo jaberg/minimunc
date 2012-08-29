4bcb590a8006a9c0334e0cc46b681d87dea76968
James Bergstra <james.bergstra@gmail.com>
2012-08-29 16:17:59 -0400

    ENH: more base packages

--- setup0.sh
+++ setup0.sh
@@ -18,5 +18,10 @@ apt-get dist-upgrade
 apt-get install firmware-realtek
 
 apt-get install nvidia-cuda-dev
-
-apt-get install 
+apt-get install gcc-4.4 g++-4.4
+apt-get install libopenblas-base libopenblas-dev liblapack3 liblapack-dev
+apt-get install libjpeg62 libjpeg62-dev
+apt-get install mongodb
+apt-get install python2.6-dev
+apt-get install python-pip
+apt-get install python-virtualenv virtualenvwrapper
