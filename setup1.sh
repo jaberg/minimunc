0ba08b06bbe52c4dc2581752dbed865726b5be72
James Bergstra <james.bergstra@gmail.com>
2012-08-29 17:24:46 -0400

    ENH: apt-get options

--- setup1.sh
+++ setup1.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
-apt-get install vim
-apt-get install git
-apt-get install firmware-realtek
-apt-get install nvidia-cuda-dev
-apt-get install gcc-4.4 g++-4.4
-apt-get install libopenblas-base libopenblas-dev liblapack3 liblapack-dev
-apt-get install libjpeg62 libjpeg62-dev
-apt-get install mongodb
-apt-get install python2.6-dev
-apt-get install python-pip
-apt-get install python-virtualenv virtualenvwrapper
+alias agi='apt-get -qq -y install'
+
+agi vim
+agi git
+agi firmware-realtek
+agi nvidia-cuda-dev
+agi gcc-4.4 g++-4.4
+agi libopenblas-base libopenblas-dev liblapack3 liblapack-dev
+agi libjpeg62 libjpeg62-dev
+agi mongodb
+agi python2.6-dev
+agi python-pip
+agi python-virtualenv
