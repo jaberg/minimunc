c04956f83813e7551be51d8ed7f9a75ed2e48df3
James Bergstra <james.bergstra@gmail.com>
2012-08-31 14:11:20 -0400

    FIX: correct python-dev version

--- setup1.sh
+++ setup1.sh
@@ -12,6 +12,6 @@ agi gcc-4.4 g++-4.4
 agi libopenblas-base libopenblas-dev liblapack3 liblapack-dev
 agi libjpeg62 libjpeg62-dev
 agi mongodb
-agi python2.6-dev
+agi python2.7-dev
 agi python-pip
 agi python-virtualenv
